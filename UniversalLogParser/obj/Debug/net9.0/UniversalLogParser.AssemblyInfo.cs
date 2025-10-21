9cb3adb29a4598ed79a24f7a23f01be768b7f514
Trambowetsky <deprydepp@gmail.com>
2025-10-22 01:04:07 +0400

    added quick filters on log entry page and changed the dropbox view

--- UniversalLogParser/obj/Debug/net9.0/UniversalLogParser.AssemblyInfo.cs
+++ UniversalLogParser/obj/Debug/net9.0/UniversalLogParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversalLogParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ebff2a16943b8fc33372f94664668fd5992d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af02b26ea5a55f68ab5b73cf8cc47b36940e1fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversalLogParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversalLogParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UniversalLogParser/wwwroot/js/site.js
+++ UniversalLogParser/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const dropArea = document.getElementById('drop-area');
+const fileInput = document.getElementById('fileInput');
+const form = document.getElementById('uploadForm');
 
-// Write your JavaScript code.
+dropArea.addEventListener('click', () => fileInput.click());
+
+dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropArea.classList.add('bg-primary', 'text-white');
+});
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('bg-primary', 'text-white');
+});
+
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropArea.classList.remove('bg-primary', 'text-white');
+    const file = e.dataTransfer.files[0];
+    uploadFile(file);
+});
+
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    uploadFile(file);
+});
+
+function uploadFile(file) {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('/LogFiles/Upload', {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else {
+            alert('Upload failed');
+        }
+    });
+}
